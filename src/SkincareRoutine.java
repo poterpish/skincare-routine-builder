b01504a81d87ed036ca3e9a00f19bc19437289df
Alua <poterpish7@gmail.com>
2026-09-11 22:04:30 +0500

    Add SkincareRoutine product

---
+++ src/SkincareRoutine.java
@@ -0,0 +1,23 @@
+public class SkincareRoutine {
+    private final String cleanser;
+    private final String toner;
+    private final String moisturizer;
+    private final String sunscreen;
+
+    SkincareRoutine(String cleanser, String toner, String moisturizer, String sunscreen) {
+        this.cleanser = cleanser;
+        this.toner = toner;
+        this.moisturizer = moisturizer;
+        this.sunscreen = sunscreen;
+    }
+
+    @Override
+    public String toString() {
+        return "SkincareRoutine{" +
+                "cleanser='" + cleanser + '\'' +
+                ", serum='" + toner + '\'' +
+                ", moisturizer='" + moisturizer + '\'' +
+                ", sunscreen='" + sunscreen + '\'' +
+                '}';
+    }
+}
