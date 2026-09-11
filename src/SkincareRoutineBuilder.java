904d8d313eecae737d390871f0b94f8076c65dcd
Alua <poterpish7@gmail.com>
2026-09-11 22:12:29 +0500

    Add SkincareRoutineBuilder interface

---
+++ src/SkincareRoutineBuilder.java
@@ -0,0 +1,9 @@
+public interface SkincareRoutineBuilder {
+    SkincareRoutineBuilder setCleanser(String cleanser);
+
+    SkincareRoutineBuilder setToner(String toner);
+
+    SkincareRoutineBuilder setMoisturizer(String moisturizer);
+
+    SkincareRoutineBuilder setSunscreen(String sunscreen);
+}
